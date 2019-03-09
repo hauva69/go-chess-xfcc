fb3517dbdd5c9b0758a7aa7c3c44b7c43c6e6432
Ari Makela <ari.makela@iki.fi>
2019-03-10 00:25:41 +0200

    date added paulKeresWasBornInXFCC

--- xfcc/date_test.go
+++ xfcc/date_test.go
@@ -5,6 +5,9 @@ import "testing"
 const paulKeresWasBornInPGN = "1916.01.07"
 const paulKeresWasBornInISO8601 = "1916-01-07"
 
+// at least the ICCF implementation has no leading zeros
+const paulKeresWasBornInXFCC = "1916-1-7"
+
 func TestEventDatePgn(t *testing.T) {
 	ed := NewEventDate()
 	t.Fatalf("FIXME: %+v", ed)
