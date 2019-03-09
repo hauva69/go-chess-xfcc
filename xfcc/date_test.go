415e401fcd34d3a6ffcd182553bd5306314ba712
Ari Makela <ari.makela@iki.fi>
2019-03-10 00:47:49 +0200

    TestEvenDatePgn => TestDatePgn

--- xfcc/date_test.go
+++ xfcc/date_test.go
@@ -1,6 +1,10 @@
 package xfcc
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 const paulKeresWasBornInPGN = "1916.01.07"
 const paulKeresWasBornInISO8601 = "1916-01-07"
@@ -8,7 +12,11 @@ const paulKeresWasBornInISO8601 = "1916-01-07"
 // at least the ICCF implementation has no leading zeros
 const paulKeresWasBornInXFCC = "1916-1-7"
 
-func TestEventDatePgn(t *testing.T) {
-	ed := NewEventDate()
-	t.Fatalf("FIXME: %+v", ed)
+func TestDatePgn(t *testing.T) {
+	expected := "1916.01.07"
+	year := 1916
+	month := 1
+	mday := 7
+	d := NewDate(&year, &month, &mday)
+	assert.Equal(t, expected, d.PGN())
 }
