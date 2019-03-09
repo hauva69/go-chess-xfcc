8d4155f1ff56a9e3698b16adf4c256ba6bea091c
Ari Makela <ari.makela@iki.fi>
2019-03-10 01:09:27 +0200

    date more date tests

--- xfcc/date_test.go
+++ xfcc/date_test.go
@@ -20,3 +20,33 @@ func TestDatePGN(t *testing.T) {
 	d := NewDate(&year, &month, &mday)
 	assert.Equal(t, expected, d.PGN())
 }
+
+func TestDatePGNYearIsNil(t *testing.T) {
+	expected := "????.01.07"
+	month := 1
+	mday := 7
+	d := NewDate(nil, &month, &mday)
+	assert.Equal(t, expected, d.PGN())
+}
+
+func TestDatePGNMonthIsNil(t *testing.T) {
+	expected := "1916.??.07"
+	year := 1916
+	mday := 7
+	d := NewDate(&year, nil, &mday)
+	assert.Equal(t, expected, d.PGN())
+}
+
+func TestDatePGNDayIsNil(t *testing.T) {
+	expected := "1916.01.??"
+	year := 1916
+	month := 1
+	d := NewDate(&year, &month, nil)
+	assert.Equal(t, expected, d.PGN())
+}
+
+func TestDatePGNDateIsNil(t *testing.T) {
+	expected := "????.??.??"
+	d := NewDate(nil, nil, nil)
+	assert.Equal(t, expected, d.PGN())
+}
