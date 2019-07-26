2a7099fd7be75051f38081e6afa259bfa99a18ae
Ari Makela <ari.makela@iki.fi>
2019-07-26 15:49:47 +0300

    date fixed parsing of the date

--- xfcc/date.go
+++ xfcc/date.go
@@ -12,7 +12,7 @@ const summerSolstice1916 = "1916-06-21"
 
 // DateRegularExpressionString is the string that is used to
 // create DateRegularExpression.
-const DateRegularExpressionString = `^(\d\d\d\d)[.-](\d\d)[.-](\d\d)$`
+const DateRegularExpressionString = `^(\d\d\d\d)[.-](\d\d?)[.-](\d\d?)$`
 
 // DateRegularExpression is used for parsin Dates from strings.
 var DateRegularExpression *regexp.Regexp
@@ -53,7 +53,8 @@ func Now() *Date {
 // "1916.01.07", "1916-01-07", 1916-1-7 or "1916.1.7" and an error.
 func Parse(s string) (*Date, error) {
 	groups := DateRegularExpression.FindAllStringSubmatch(s, -1)
-	if len(groups[0]) != 4 {
+	log.Printf("parsing %q, got %+v", s, groups)
+	if len(groups) != 1 || len(groups[0]) != 4 {
 		return nil, fmt.Errorf(
 			"parsing the date failed: %q does not match %q",
 			s,
