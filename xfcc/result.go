8f4677254be6aeecd0b24daa928e1344fdb128d0
Ari Makela <ari.makela@iki.fi>
2019-08-25 09:22:53 +0300

    result work on result types

--- xfcc/result.go
+++ xfcc/result.go
@@ -1,4 +1,36 @@
 package xfcc
 
-// Result models the result of the game.
+// Result models the result of the game. It can have following values:
+// - "Ongoing"
+// - "WhiteWins"
+// - "BlackWins"
+// - "Draw"
+// - "WhiteWinAdjudicated"
+// - "BlackWinAdjudicated"
+// - "DrawAdjudicated"
+// - "WhiteDefaulted"
+// - "BlackDefaulted"
+// - "BothDefaulted"
+// - "Cancelled"
+// - "AdjudicationPending"
 type Result string
+
+// PGN returns the result of the game as a PGN string.
+func (r *Result) PGN() (string, error) {
+	/*
+		"Ongoing"
+		"WhiteWins"
+		"BlackWins"
+		"Draw"
+		"WhiteWinAdjudicated"
+		"BlackWinAdjudicated"
+		"DrawAdjudicated"
+		"WhiteDefaulted"
+		"BlackDefaulted"
+		"BothDefaulted"
+		"Cancelled"
+		"AdjudicationPending"
+	*/
+
+	return string(*r), nil
+}
