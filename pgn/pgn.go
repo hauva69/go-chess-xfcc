5abbd8609863e570e30af2fef704652fee17027b
Ari Makela <ari.makela@iki.fi>
2019-11-19 21:14:02 +0200

    endgametablebases added Game()

--- pgn/pgn.go
+++ pgn/pgn.go
@@ -7,13 +7,23 @@ import (
 	"github.com/notnil/chess"
 )
 
-// FEN returns the final position of the game as a FEN string.
-func FEN(pgn string) (string, error) {
+// Game returns the game from a PGN string
+func Game(pgn string) (*chess.Game, error) {
 	gameFunc, err := chess.PGN(strings.NewReader(pgn))
 	if err != nil {
 		log.Print(err)
-		return "ERROR", err
+		return nil, err
 	}
 
-	return chess.NewGame(gameFunc).FEN(), nil
+	return chess.NewGame(gameFunc), nil
+}
+
+// FEN returns the final position of the game as a FEN string.
+func FEN(pgn string) (string, error) {
+	game, err := Game(pgn)
+	if err != nil {
+		return "", err
+	}
+
+	return game.FEN(), nil
 }
