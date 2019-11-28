d192a2d0c3ddddb7568463f16f979c941f5afe0a
Ari Makela <ari.makela@iki.fi>
2019-11-28 16:04:13 +0200

    endgametablebases first commit

---
+++ pgn/pgn_test.go
@@ -0,0 +1,13 @@
+package pgn
+
+// FewPiecesGame1 is something to be fixed
+// FIXME remove links to the test data
+// FIXME move these to a separate test file
+// https://www.iccf.com/game?id=1086957[Event "FIN/AC/4 (FIN)"[Date "2019.04.10"]
+// [Round "-"]
+// [White "Mäkelä, Ari"]
+// [Black "Rajala, Olavi"]
+// [Result "1-0"]
+// [WhiteElo "2181"]
+// [BlackElo "1995"]
+const FewPiecesGame1 = `1.Nf3 e6 2.c4 Nf6 3.Nc3 c5 4.g3 Nc6 5.Bg2 Qb6 6.O-O Be7 7.b3 d5 8.Na4 Qa6 9.Bb2 O-O 10.Bxf6 Bxf6 11.cxd5 exd5 12.Nxc5 Qa5 13.Rc1 Bb2 14.Rb1 Ba3 15.d4 Bf5 16.Nd3 Rac8 17.Nh4 Bxd3 18.Qxd3 Nb4 19.Qf3 g6 20.Bh3 Rc2 21.Ng2 Rd8 22.Ne3 Rc6 23.Bg2 Qc7 24.h4 b5 25.h5 f5 26.Nc4 dxc4 27.bxc4 Rxc4 28.hxg6 hxg6 29.Qxa3 a5 30.Qf3 Nc2 31.d5 Kg7 32.Qd3 Qc5 33.Rfc1 Nd4 34.Rxc4 Qxc4 35.Qxc4 bxc4 36.Kf1 Kf6 37.Rb6+ Kf7 38.Rb7+ Kf6 39.e3 Nc2 40.a4 c3 41.Rc7 Rb8 42.Rxc3 Rb1+ 43.Ke2 Nb4 44.f4 Rb2+ 45.Kf1 Rb1+ 46.Kf2 Rb2+ 47.Kg1 Ra2 48.e4 fxe4 49.Bxe4 Rxa4 50.Rc4 Ra2 51.Rd4 Nc2 52.Rd1 Ke7 53.d6+ Kd8 54.Bc6 Nb4 55.Bb5 Rc2 56.Re1 Nc6 57.Re6 Rc5 58.Bf1 a4 59.Rxg6 Rc1 60.Kf2 Kc8 61.Bh3+ Kb7 62.Rg7+ Kb6 63.Rg8 a3 64.Ra8 Rc2+ 65.Ke3 Rc3+ 66.Ke4 Rxg3 67.Be6 Rg6 68.Kf5 Rg3 69.d7 Rd3 70.Ke4 Nb4 71.Rb8+ Kc5 72.Rxb4 Rxd7 73.Bxd7 Kxb4 1-0`
